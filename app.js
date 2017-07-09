50c14c02080c91b6c1185ae03680185cae141a7c
kaitinghuang <kightingh@gmail.com>
2017-07-09 15:17:16 -0700

    front-end setup

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(cors());
 
+//Allows node to find static content
+app.use(express.static(__dirname + '/public'));
+
+
+//Create the database entry
+//If we call http://, the system will think that we'll go into a sub directory. (*) is saying that "accept 'all' as the string regardless how it's formatted"
+app.get('/new/:urlToShorten(*)', function(req, res, next){
+    var urlToShorten = req.params.urlToShorten;
+    console.log(urlToShorten);
+});
+
+
 
 
 
